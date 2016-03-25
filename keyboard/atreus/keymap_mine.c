f2a7300f483c7679af318d65d441dfb6ad610d3c
George Madrid <gmadrid@gmail.com>
2016-03-24 21:32:16 -0400

    Adding my build defaults to the Makefile. Also, moving CMD key and RALT key to be a little more like Kinesis.

--- keyboard/atreus/keymap_mine.c
+++ keyboard/atreus/keymap_mine.c
@@ -10,8 +10,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(KC_Q, KC_W, KC_E, KC_R, KC_T,         KC_Y, KC_U, KC_I,    KC_O,   KC_P, \
          KC_A, KC_S, KC_D, KC_F, KC_G,         KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, \
          KC_FN5, KC_X, KC_C, KC_V, KC_B,         KC_N, KC_M, KC_COMM, KC_DOT, KC_FN4, \
-         KC_ESC, KC_TAB, KC_LGUI, KC_LALT, KC_BSPC, KC_LCTL, \
-                                     KC_LGUI,  KC_SPC, KC_FN0, KC_MINS, KC_QUOT, KC_ENT), \
+         KC_ESC, KC_TAB, KC_LSFT, KC_LGUI, KC_BSPC, KC_LCTL, \
+                                     KC_RALT,  KC_SPC, KC_FN0, KC_MINS, KC_QUOT, KC_ENT), \
   /* 1: punctuation and numbers */
   FN_ARROW_LAYER,                                     \
   /* 2: arrows and function keys */
